017c969aaeb6290b22a6ccf0bc78231d1ba0698d
MNA0786 <ansarimahtabco41513@gmail.com>
2026-03-31 18:35:49 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Set Apache to listen on Render's PORT environment variable
-if [ -n "$PORT" ]; then
-    sed -i "s/Listen 80/Listen $PORT/g" /etc/apache2/ports.conf
-    sed -i "s/:80>/:$PORT>/g" /etc/apache2/sites-available/000-default.conf
-fi
-# Start Apache in foreground
-apache2-foreground
